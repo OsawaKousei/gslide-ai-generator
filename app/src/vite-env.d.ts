66d5d3b5327c298a4814b941649eae1e059c9a78
OsawaKousei <jwithelis@gmail.com>
2026-01-19 00:06:07 +0900

    リンタエラーを修正

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,11 +1,15 @@
 /// <reference types="vite/client" />
-import { z } from 'zod';
-import { envSchema } from './env';
+import type { z } from 'zod';
+import type { envSchema } from './env';
 
-type Env = z.infer<typeof envSchema>;
+type Env = z.infer<envSchema>;
 
-interface ImportMetaEnv extends Env {}
+/* eslint-disable no-restricted-syntax, @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-object-type */
+declare global {
+  interface ImportMetaEnv extends Env {}
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
 }
+/* eslint-enable */
